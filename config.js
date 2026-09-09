8f292c76dcb1ea923ae2a435220a8d0f79d503f2
tarotuniverso2026 <tarot.universo2026@gmail.com>
2026-09-09 09:14:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.TAROTEAME_CONFIG = {
   SUPABASE_URL: "https://ozbbgfxixbgffxnhqpzh.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_Xag7GClzJc37vg2DQXhBVA_QfU0NUyd"
+VAPID_PUBLIC_KEY: BMSqsZpdtxO_VZSIBadA65JyA3oy6sCUvX8x3VjFnGLucq71kd4zpJHN6xUegy0udibpY9lO2C6yiIqKvP1uBEo
 };
